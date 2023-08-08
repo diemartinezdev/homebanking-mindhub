0ee2a1b42fdcf78a9d7aebed5dfe6540f32adc2e
Diego Martinez <martinez.diego90@gmail.com>
2023-08-09 00:21:53 +0200

    add initial project

---
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,55 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Client() { }
+
+    public Client(String first, String last, String mail) {
+        this.firstName = first;
+        this.lastName = last;
+        this.email = mail;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
