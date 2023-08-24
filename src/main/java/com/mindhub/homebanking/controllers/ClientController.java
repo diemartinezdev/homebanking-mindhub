8b687aa769b52437b5bc482b93dd69d49246053f
Diego Martinez <martinez.diego90@gmail.com>
2023-08-24 12:30:10 +0200

    modify client controller

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -21,6 +21,8 @@ public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping("/clients")
     private List<ClientDTO> getClients() {
@@ -32,14 +34,27 @@ public class ClientController {
         return new ClientDTO(clientRepository.findById(id).orElse(null));
     }
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @RequestMapping(path = "/clients", method = RequestMethod.POST)
     public ResponseEntity<Object> register(
             @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password
     ) {
-        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        if (firstName.isEmpty()) {
+            return new ResponseEntity<>("Please complete the first name", HttpStatus.FORBIDDEN);
+        } else if (lastName.isEmpty()) {
+            return new ResponseEntity<>("Please complete the last name", HttpStatus.FORBIDDEN);
+        } else if (email.isEmpty()) {
+            return new ResponseEntity<>("Please complete the email", HttpStatus.FORBIDDEN);
+        } else if (password.isEmpty()) {
+            return new ResponseEntity<>("Please complete the password", HttpStatus.FORBIDDEN);
+        } else if (clientRepository.findByEmail(email) !=  null) {
+            return new ResponseEntity<>("Email already in use", HttpStatus.FORBIDDEN);
+        }
+            Client newClient = new Client(firstName, lastName, email, passwordEncoder.encode(password));
+            clientRepository.save(newClient);
+            return new ResponseEntity<>("Registration completed!",HttpStatus.CREATED);
+    }
+       /*
+       if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
             return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
         }
         if (clientRepository.findByEmail(email) !=  null) {
@@ -47,7 +62,7 @@ public class ClientController {
         }
         clientRepository.save(new Client(firstName, lastName, email, passwordEncoder.encode(password)));
         return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+        */
 
     @RequestMapping("/clients/current")
     public ClientDTO getCurrent (Authentication authentication) {
