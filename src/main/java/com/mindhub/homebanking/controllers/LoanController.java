c8884c885e6d2a557976a006993c5f5d3c1de633
Diego Martinez <martinez.diego90@gmail.com>
2023-09-04 12:48:56 +0200

    modify loan controller

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -9,6 +9,7 @@ import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import com.mindhub.homebanking.repositories.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -37,12 +38,33 @@ public class LoanController {
     public ResponseEntity<Object> createLoan(
             @RequestBody LoanApplicationDTO loanApplicationDTO, Authentication authentication
             ) {
-    Client client = clientRepository.findByEmail(authentication.getName());
-    Loan loan = loanRepository.findByName(loanApplicationDTO.getLoanId());
-    Account account = accountRepository.findByNumber(loanApplicationDTO.getDestinationAccount());
+        Client client = clientRepository.findByEmail(authentication.getName());
+        Loan loan = loanRepository.findByName(loanApplicationDTO.getLoanId());
+        Account destinyAccount = accountRepository.findByNumber(loanApplicationDTO.getDestinationAccount());
 
-    if (client == null) {
-        return new ResponseEntity<>("")
-    }
+        if (loanApplicationDTO.getLoanId() == null || loanApplicationDTO.getAmount() == null || loanApplicationDTO.getPayments() == null || loanApplicationDTO.getDestinationAccount() == null) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getAmount() <= 0) {
+            return new ResponseEntity<>("Incorrect amount", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getPayments() <= 0) {
+            return new ResponseEntity<>("Incorrect number of payments", HttpStatus.FORBIDDEN);
+        }
+        if (loan == null) {
+            return new ResponseEntity<>("Loan doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getAmount() > loan.getMaxAmount()) {
+            return new ResponseEntity<>("Amount requested exceeds the maximum of the loan", HttpStatus.FORBIDDEN);
+        }
+        if (!loan.getPayments().contains(loanApplicationDTO.getPayments())) {
+            return new ResponseEntity<>("Payments requested exceeds the maximum of the loan", HttpStatus.FORBIDDEN);
+        }
+        if (destinyAccount == null) {
+            return new ResponseEntity<>("Destiny account doesn't exist", HttpStatus.FORBIDDEN);
+        }
+        if (!client.getAccounts().contains(destinyAccount)) {
+            return new ResponseEntity<>("The account doesn't correspond to the client", HttpStatus.FORBIDDEN);
+        }
     }
 }
